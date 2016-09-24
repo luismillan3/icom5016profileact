9442a142cafb03caf21c14e5d078d1ec6b4e0911
Specialist17 <Specialist@Fernandos-MacBook-Pro-5.local>
2016-09-24 15:30:11 -0400

    creating file structure with pages and controllers

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.static(__dirname + '/views'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/views/index.html');
--- views/js/app.js
+++ views/js/app.js
@@ -1,4 +1,6 @@
-angular.module('app', ['ngRoute', 'controller'])
+angular.module('app', ['ngRoute', 'controller', 'professorController', 'professorResearchController',
+	'recruiterEventController', 'recruiterSearchStudentController', 'researchController', 'signInController', 'signUpController',
+	 'studentEventsController', 'studentProfileController', 'studentResearchController'])
 
 .config(function($routeProvider) {
 	$routeProvider
@@ -7,15 +9,15 @@ angular.module('app', ['ngRoute', 'controller'])
 	})
 	.when('/signin', {
 		templateUrl: 'templates/sign_in.html',
-		controller: 'BlogCtrl'
+		controller: 'signInController'
 	})
     .when('/research', {
 		templateUrl: 'templates/research.html',
-		controller: 'ResearchCtrl'
+		controller: 'researchController'
 	})
     .when('/signup', {
 		templateUrl: 'templates/sign_up.html',
-		controller: 'SignUpCtrl'
+		controller: 'signUpController'
 	})
 	.otherwise({
 		templateUrl: 'templates/404.html'
