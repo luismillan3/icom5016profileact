81376edbd172a5084d9242ca73b530317e1333cf
Wilfredo <wilfredo.montanez@upr.edu>
2016-09-25 15:49:53 -0400

    Added Recruiter Event need datetimepicker

--- views/js/Recruiter.js
+++ views/js/Recruiter.js
@@ -9,8 +9,25 @@ angular.module('recruiter',[])
 }]).controller('recruiterEventController', ['$scope', '$http', '$log', function($scope, $http, $log) {
 
     $scope.frmToggle = function() {
-        $('#blogForm').slideToggle();
+        $('#eventForm').slideToggle();
     }
+    $scope.event=[];
+     $scope.events = [
+        { title: 'Master HTML/CSS/Javascript',description:"Awesome Event",date:"10/12/16 10:00"},
+        { title: 'Raytheon Session',description:"Awesome Event",date:"10/12/16 10:00"},
+        { title: 'Johns Hopkins Session',description:"Awesome Event",date:"10/12/16 10:00"},
+        { title: 'Boeing Company',description:"Awesome Event",date:"10/12/16 10:00"},
+        { title: 'Mario Bros in the House',description:"Awesome Event",date:"10/12/16 10:00"}
+      ]
+     $scope.addEvent=function(e){
+     	$scope.events.push(e);
+     	$scope.event=[];
+     }
+     $scope.removeEvent=function(e){
+     	for(var i = $scope.events.length-1; i--;){
+	if ($scope.events[i] === e) $scope.events.splice(i, 1);
+}
+     }
 
 }]).controller('recruiterSearchController', ['$scope', '$http', '$log', function($scope, $http, $log) {
 
