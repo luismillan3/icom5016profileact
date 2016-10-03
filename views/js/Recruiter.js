d71c82958e5b668511521d6c8dd29e50fc3b3f8d
Wilfredo <wilfredo.montanez@upr.edu>
2016-10-02 20:34:57 -0400

    Added searchStudents page in recruiters

--- views/js/Recruiter.js
+++ views/js/Recruiter.js
@@ -2,7 +2,7 @@ angular.module('recruiter',[])
 
 .controller('recruiterProfileController', ['$scope', '$http', '$log', function($scope, $http,$location, $log) {
 
-	
+	$scope.newr={}
     $scope.recruiter={name:"Alejandro",lastname:"Martinez",email:"ale.ma@raytheon.com",company:"Raytheon"}
     $scope.pic="public/dummyprofilepic.png"
     $scope.frmToggle = function() {
@@ -14,6 +14,7 @@ angular.module('recruiter',[])
 	}	
     $scope.updateProfile=function(info){
     	$scope.recruiter=info;
+    	$scope.newr={}
     }
 
 }]).controller('recruiterEventController', ['$scope', '$http', '$log', function($scope, $http, $log) {
@@ -22,7 +23,7 @@ angular.module('recruiter',[])
         $('#eventForm').slideToggle();
     }
     $scope.event=[];
-     $scope.events = [
+    $scope.events = [
         { title: 'Master HTML/CSS/Javascript',description:"Awesome Event",date:"10/12/16 10:00"},
         { title: 'Raytheon Session',description:"Awesome Event",date:"10/12/16 10:00"},
         { title: 'Johns Hopkins Session',description:"Awesome Event",date:"10/12/16 10:00"},
@@ -40,9 +41,46 @@ angular.module('recruiter',[])
      }
 
 }]).controller('recruiterSearchController', ['$scope', '$http', '$log', function($scope, $http, $log) {
-
-    $scope.frmToggle = function() {
-        $('#blogForm').slideToggle();
-    }
+	$scope.results=[]
+	$scope.criteria={}
+	$scope.majors=[{value:"ICOM"},
+		{value:"INEL"},
+		{value:"INSO"},
+		{value:"INME"},
+		{value:"INCI"},
+		{value:"ININ"},
+		{value:"INQU"}]
+	  $scope.students = [
+        { studentName: 'Marcel',studentLastName:"Fuentes",gpa:3.53,major:"BIOL"},
+        { studentName: 'Maria',studentLastName:"Del Valle",gpa:3.32,major:"COMP"},
+        { studentName: 'Kelvin',studentLastName:"Pelota",gpa:2.74,major:"ICOM"},
+        { studentName: 'Nerymar',studentLastName:"Cucuza",gpa:2.9,major:"INEL"},
+        { studentName: 'Cafralin',studentLastName:"Pelora",gpa:3.94,major:"ININ"}
+      ]
+      $scope.results=$scope.students
+      $scope.searchStudents=function(criteria){
+      	$scope.results=[]
+      	$scope.criteria={}
+      	for(var i=0;i<$scope.students.length;i++){
+      		
+      		// if(criteria==null)break;
+      		if(criteria.gpa!=null &&criteria.major!=null){
+      			if ($scope.students[i].major==criteria.major&&$scope.students[i].gpa>=criteria.gpa){
+      				$scope.results.push($scope.students[i])
+      		}
+      		}
+      		else if(criteria.gpa==null &&criteria.major!=null){
+      			if ($scope.students[i].major==criteria.major){
+      				$scope.results.push($scope.students[i])
+      		}
+      		}
+      		else if(criteria.gpa!=null &&criteria.major==null){
+      			if ($scope.students[i].gpa>=criteria.gpa){
+      				$scope.results.push($scope.students[i])
+      		}
+      		}
+      		
+      	}
+      }
 
 }])
