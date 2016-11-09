606282584d319cbdcd212cbc002c3e02fef1d184
luismillan3 <luis.millan3@upr.edu>
2016-11-09 16:49:34 -0400

    View engine

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/views/index.html');
