dfebec0ba441a88efc61495a305d58df78804792
luismillan3 <luis.millan3@upr.edu>
2016-12-04 20:52:47 -0400

    Second try

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/views'));
 
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, './uploads/');
   },
   filename: function (req, file, callback) {
     callback(null, file.fieldname + '-' + Date.now());
